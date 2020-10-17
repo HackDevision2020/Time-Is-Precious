62487fe686cd48e2c4335a4df84e5e95546d370d
Jamee Kim <jameekim@nodaplife.me>
2020-10-16 21:42:20 -0400

    Splash screen integration
    - Integration of the splash screen to the stage progression system
    - Proper interactable scripts to add door to the splash screen scene

---
+++ Assets/Scripts/Items/SetActive.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace Items
+{
+    public class SetActive : MonoBehaviour
+    {
+        public GameObject target;
+
+        public SetActiveMode mode;
+
+        public void OnInteraction()
+        {
+            switch (mode)
+            {
+                case SetActiveMode.Toggle:
+                    target.SetActive(!target.activeSelf);
+                    break;
+
+                case SetActiveMode.Activate:
+                    target.SetActive(true);
+                    break;
+
+                case SetActiveMode.Deactivate:
+                    target.SetActive(false);
+                    break;
+            }
+        }
+
+        public enum SetActiveMode
+        {
+            Toggle,
+            Activate,
+            Deactivate,
+        }
+    }
+}
