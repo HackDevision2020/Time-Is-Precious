ccf17099ce92f117d545ab3ada373c4f4d8dcb67
Jerome <nathan_9_9@hotmail.com>
2020-11-20 15:12:15 -0500

    Edit Level 3 Items
    Going to be experimenting on stuff so backing it up

---
+++ Assets/Scripts/StuffFromClass/DoorItemTrigger.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorItemTrigger : MonoBehaviour
+{
+    [SerializeField] private GameObject door; 
+    private static DoorItemTrigger _instance;
+    public static DoorItemTrigger Instance { get { return _instance; } }
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        if(_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    private int totalItems = 0, itemsCollected = 0;
+    void Start()
+    {
+        totalItems = GameObject.FindGameObjectsWithTag("Item").Length;
+        Debug.Log(totalItems);
+    }
+    public void PickedUpItem()
+    {
+        itemsCollected++;
+        Debug.Log("Items collected " + itemsCollected);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(totalItems == itemsCollected)
+        {
+            Destroy(door);
+        }
+    }
+}
